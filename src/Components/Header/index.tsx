c6be364ec4175867bf05bd3ea93efe9589cf5ce8
gabrielfalanga <gabrielmartinsfalanga@gmail.com>
2024-09-16 17:39:55 -0300

    header reformulado, responsivo

--- src/Components/Header/index.tsx
+++ src/Components/Header/index.tsx
@@ -4,15 +4,13 @@ import usuario from '../../assets/img/usuario.png';
 
 const Header: React.FC = () => {
     return (
-        <header className={styles.cabecalho}>
-      <div className={styles.divLogo}>
-        <a href="/">
-          {/* <img id={styles.logo} src={logo} alt="Logo da Porto" /> */}
-          <h1>Auto<b>Care</b></h1>
-        </a>
-      </div>
+      <header className={styles.cabecalho}>
+        <div className={styles.divLogo}>
+          <a href="/">
+            <h1>Auto<b>Care</b></h1>
+          </a>
+        </div>
 
-      <div className={styles.navEuser}>
         <nav>
           <ul>
             <li>
@@ -21,15 +19,15 @@ const Header: React.FC = () => {
               </a>
             </li>
             <li>
-             <a href="/ajuda">
-               Ajuda
-             </a>
+              <a href="/ajuda">
+                Ajuda
+              </a>
             </li>
             <li>
-             <a href="/participantes">
-               Participantes
-             </a>
-           </li>
+              <a href="/participantes">
+                Participantes
+              </a>
+            </li>
           </ul>
         </nav>
 
@@ -37,10 +35,8 @@ const Header: React.FC = () => {
           <a href="/login">
             <img id={styles.imgUsuario} src={usuario} alt="Imagem de Pessoa representando o Usuário"/>
           </a>
-       </div>
-
-      </div>
-    </header>
+        </div>
+      </header>
     )
 }
 
